75bd27ff0ce1cf0b70a7a2d5a337f8dc7ee97636
Ewan Jones <ewan.jones@octoenergy.com>
2024-12-22 22:58:29 +0000

    Add more data to assets

--- src/capture/interfaces/assets.py
+++ src/capture/interfaces/assets.py
@@ -21,7 +21,21 @@ async def get_property_and_assets():
         capacity_kwh=9,
         events=[event_1],
     )
+    event_2 = assets.ChargeEvent(
+        occurred_at=dt.datetime(2024, 12, 22, 9),
+        direction=Direction.IMPORT,
+        charge_percent=68,
+    )
+    asset_2 = assets.Asset(
+        serial_number="BAT002",
+        voltage=20,
+        current=60,
+        capacity_kwh=7,
+        events=[event_2],
+    )
     _property = assets.Property(
-        address="213A Lower Clapton Road, London, E5 8EG", user=None, assets=[asset_1]
+        address="213A Lower Clapton Road, London, E5 8EG",
+        user=None,
+        assets=[asset_1, asset_2],
     )
     return [_property]
